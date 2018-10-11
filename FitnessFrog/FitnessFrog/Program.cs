baa33e9c63183511d62e9c96a66bcaad870634bb
Sean Steberis <sasteberis@hotmail.co.uk>
2018-10-11 13:43:08 +0100

    Initial commit

---
+++ FitnessFrog/FitnessFrog/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessFrog
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Prmpt the user for minutes exercised
+            // Add minutes exercised to total
+            // Display total minutes excersied to the screen
+            // Repeat until the user quits
+
+
+        }
+    }
+}
